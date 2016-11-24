2eb4321676032c0bba9497e4e9ab7ea94c14c775
sabinakaczor <sabina.gbw@gmail.com>
2016-11-24 17:15:25 +0100

    24.12

--- kolo_fortuny.py
+++ kolo_fortuny.py
@@ -2,23 +2,14 @@
 
 import random
 
-
-
 class Game:
     def __init__(self):
         self.name = "Koło fortuny"
 
-    def get_users_number(self):
-        self.users = input("Ile osób gra?\n")
-        return self.users
-
-    def get_names(self):
-        names = []
-        for i in range (0,self.users):
-            names[i] = raw_input("Imię " + str(i-1) + ". uczestnika: ")
-
-    def available_pos(self):
-        self.list_av = range(1,self.users+1)
+    def get_players_number(self):
+        self.players_no = input("Ile osób gra?\n")
+        self.list_av = range(1, self.players_no + 1)
+        return self.players_no
 
     def get_category(self):
         file_categories = open("kategorie.txt","r")
@@ -34,22 +25,27 @@ class Game:
         i = random.randint(1,len(passwds)-1)
         self.passwd = passwds[i]
 
-class User:
-    def __init__(self,name):
-        self.score = 0
-        self.name = name
+    def start_writing(self):
+        with open("scores.txt","w") as fw:
+            fw.write("Uczestnik Wynik")
 
+class Player:
+    def __init__(self,g):
+        self.score = 0
+
+    def get_name(self,i):
+            self.name = raw_input("Imię " + str(i) + ". uczestnika: ")
 
     def get_pos(self):
         random.seed()
-        self.pos = random.randint(1,Game.users)
-        Game.list_av.remove(self.pos)
+        self.pos = random.choice(g.list_av)
+        g.list_av.remove(self.pos)
         return self.pos
 
     def twist(self):
         random.seed()
         self.rand_no = random.randint(-10,10)
-        print "Wylosowałeś " + self.rand_no +"\n"
+        print "\n" + self.name + ":\nWylosowałeś " + str(self.rand_no)
         if self.rand_no != 0:
             self.get_letter()
         else:
@@ -59,40 +55,60 @@ class User:
         letter = raw_input("Podaj literkę ")
         while (len(letter) != 1):
             letter = raw_input("Podaj pojedynczą literkę ")
-        if letter in self.passwd:
+        if letter in g.passwd:
             k = 0
-            for i in range (0,len(self.passwd)):
-                if self.passwd[i] == letter:
+            for i in range (0,len(g.passwd)):
+                if g.passwd[i] == letter:
                     k += 1
-            # cos zrobić z k
             if self.rand_no > 0:
                 self.score += self.rand_no
-        self.guess_passwd()
+            self.guess_passwd(k)
+        else:
+            print "\nHasło nie zawiera tej literki!"
 
-    def guess_passwd(self):
+    def guess_passwd(self,k):
         guess = raw_input("Jakie hasło obstawiasz?\n")
-        if guess==self.passwd:
-            self.score *= len(self.passwd) - 1
+        if guess==g.passwd:
+            self.score *= len(g.passwd) - k
+            print "Brawo!"
+        else:
+            print "Niestety, nie zgadłeś!"
 
-    def get_score(self):
-        if self.rand_no > 0 or self.rand_no < 0:
-            self.score += self.rand_no
-            self.get_letter()
-        elif self.rand_no == 0:
-            self.score = 0
+    def write_score(self):
+        with open("scores.txt", "a") as fw:
+            fw.write("\n" + self.name + " " + self.score)
 
-
+def byPos_key(player):
+    return player.pos
 
 g = Game()
-n = g.get_users_number()
-g.get_names()
-g.available_pos()
+
+n = g.get_players_number()
 g.get_category()
 g.get_passwd()
+g.start_writing()
 
-users = []
-for i in range(0,n):
-    users[i] = User()
+print "Hasło: " + g.passwd
+#h = ['*' for x ]
 
-for j in range (0,n):
-    users[i].
+
+players = []
+for i in range (0,g.players_no):
+    players.append(Player(g))
+
+for i in range (0,len(players)):
+    players[i].get_name(i+1)
+    players[i].get_pos()
+
+players = sorted(players, key= byPos_key)
+for player in players:
+    print str(player.pos) + " " + player.name
+
+for i in range (0,len(players)):
+    players[i].twist()
+
+for i in range (9,len(players)):
+    players[i].write_score()
+
+#print players
+
