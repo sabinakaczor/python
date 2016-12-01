f68c1ae68e8a13039c11ea96a00f088ecd4abb71
sabinakaczor <sabina.gbw@gmail.com>
2016-12-01 16:53:20 +0100

    Koło fortuny - aktualizacja

--- kolo_fortuny.py
+++ kolo_fortuny.py
@@ -5,6 +5,8 @@ import random
 class Game:
     def __init__(self):
         self.name = "Koło fortuny"
+        self.guessed = []
+        self.done = False
 
     def get_players_number(self):
         self.players_no = input("Ile osób gra?\n")
@@ -21,13 +23,26 @@ class Game:
     def get_passwd(self):
         file_passwds = open("hasla.txt","r")
         passwds = file_passwds.readlines()
-        random.seed()
-        i = random.randint(1,len(passwds)-1)
-        self.passwd = passwds[i]
+        for i in range(0,len(passwds)):
+            if self.category[:-1] in passwds[i]:
+                random.seed()
+                j = random.randint(i+1, i+6)
+                self.passwd = passwds[j][:-1]
+
 
     def start_writing(self):
         with open("scores.txt","w") as fw:
             fw.write("Uczestnik Wynik")
+
+    def show_passwd(self):
+        copy = list(self.passwd)
+        for i in range (0,len(copy)):
+            if copy[i] not in self.guessed:
+                copy[i] = "*"
+        copy1 = ""
+        for i in range(0, len(copy)):
+            copy1 += str(copy[i])
+        return copy1
 
 class Player:
     def __init__(self,g):
@@ -56,6 +71,8 @@ class Player:
         while (len(letter) != 1):
             letter = raw_input("Podaj pojedynczą literkę ")
         if letter in g.passwd:
+            g.guessed.append(letter)
+            print "\nHasło: " + g.show_passwd()
             k = 0
             for i in range (0,len(g.passwd)):
                 if g.passwd[i] == letter:
@@ -65,31 +82,39 @@ class Player:
             self.guess_passwd(k)
         else:
             print "\nHasło nie zawiera tej literki!"
+            self.score += self.rand_no
 
     def guess_passwd(self,k):
         guess = raw_input("Jakie hasło obstawiasz?\n")
         if guess==g.passwd:
             self.score *= len(g.passwd) - k
-            print "Brawo!"
+            print "\nBrawo!"
+            g.done = True
         else:
-            print "Niestety, nie zgadłeś!"
+            print "\nNiestety, nie zgadłeś!"
 
     def write_score(self):
         with open("scores.txt", "a") as fw:
-            fw.write("\n" + self.name + " " + self.score)
+            fw.write("\n" + "%9s" % self.name + " %5s" % str(self.score))
+
+def code_passwd(passwd,letter):
+    for x in passwd:
+        if x != letter:
+            x = '*'
+
 
 def byPos_key(player):
     return player.pos
 
 g = Game()
 
+print g.name + "\n"
 n = g.get_players_number()
 g.get_category()
 g.get_passwd()
 g.start_writing()
 
-print "Hasło: " + g.passwd
-#h = ['*' for x ]
+
 
 
 players = []
@@ -100,15 +125,20 @@ for i in range (0,len(players)):
     players[i].get_name(i+1)
     players[i].get_pos()
 
+print "\nKolejność grania:"
 players = sorted(players, key= byPos_key)
 for player in players:
     print str(player.pos) + " " + player.name
 
-for i in range (0,len(players)):
-    players[i].twist()
+print "\nKategoria: " + g.category + "Hasło: " + g.show_passwd()
 
-for i in range (9,len(players)):
+while(g.done == False):
+    for i in range (0,len(players)):
+        players[i].twist()
+        if (g.done):
+            break
+
+for i in range (0,len(players)):
     players[i].write_score()
 
-#print players
-
+print "Wyniki zapisano do pliku scores.txt."
